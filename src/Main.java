23d70a406905726694ce7d7dde0831228e81a2df
USER <olis_chan@mail.ru>
2023-05-16 14:20:39 +0300

    yjvyy

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Object[] pole = new Object[10];
-        pole[0]= new Wolf("вася", 10);
+        pole[0]= new Wolf("вас", 10);
         pole[5]= new Rabbit("пет", 5);
 
 
